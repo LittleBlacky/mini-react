5870e494a1e63d7c866400e69ec8cd383201b545
LittleBlacky <805399080@qq.com>
2026-01-08 16:23:53 +0800

    feat: 添加render函数实现DOM渲染功能
    实现将虚拟DOM渲染到真实DOM的功能，支持递归处理子节点

--- main.js
+++ main.js
@@ -8,4 +8,22 @@ function createElement(tag, props, ...children) {
     }
 }
 
+function render(element, container) {
+    const { tag, props } = element;
+    const dom = document.createElement(tag);
+    for (const key in props) {
+        if (key === 'children') {
+            props[key].forEach(child => render(dom, child));
+        } else {
+            dom[key] = props[key];
+        }
+    }
+    container.appendChild(dom);
+}
+
+export {
+    createElement,
+    render,
+};
+
 export default MiniReact;

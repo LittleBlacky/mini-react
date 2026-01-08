7b40d6fc0fb0a654fde0efa757a778a5efe143e7
LittleBlacky <805399080@qq.com>
2026-01-08 16:44:04 +0800

    feat: 创建createTextElement，特殊处理普通类型(number、 string等)节点

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
+const TEXT_ELEMENT = 'TEXT_ELEMENT'
+
 function createElement(type, props, ...children) {
     return {
         type,
         props: {
             ...props,
-            children
+            children: children.map((child) => typeof child === 'object' ? child : createTextElement(child))
         },
+    }
+}
+
+function createTextElement(text) {
+    return {
+        type: TEXT_ELEMENT,
+        props: {
+            nodeValue: text,
+            children: []
+        }
     }
 }
 
 function render(element, container) {
     const { type, props } = element;
-    const dom = document.createElement(tag);
-    for (const key in props) {
-        if (key === 'children') {
-            props[key].forEach(child => render(dom, child));
-        } else {
-            dom[key] = props[key];
-        }
-    }
+    const dom = type === TEXT_ELEMENT ? document.createTextNode('') : document.createElement(tag);
+    const isProperty = key => key !== "children";
+    Object.keys(props).filter(isProperty).forEach(key => dom[key] = props[key]);
+    props['children'].forEach((child) => render(child, dom));
     container.appendChild(dom);
 }
 

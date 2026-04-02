9ba2114f0e3331118be67af9629f28d4bc98ecba
LittleBlacky <805399080@qq.com>
2026-04-02 16:19:34 +0800

    feat: 实现performUnitOfWork，用于协调和处理 Fiber 树中的每一个“工作单元”（Unit of Work），即每一个 Fiber 节点

--- src/mini-react.js
+++ src/mini-react.js
@@ -35,6 +35,38 @@ function render(element, container) {
   container.appendChild(dom);
 }
 
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createElement(fiber);
+  }
+  const children = fiber.props.children;
+  let prevSibling = null;
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      parent: fiber,
+      dom: null,
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+  });
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+}
+
 export const MiniReact = {
   createElement,
   render,
